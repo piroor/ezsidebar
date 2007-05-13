afc724f9ec4dd6e3e432764bdb06de7a1ef8e82a
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2007-05-13 20:36:01 +0000

    拡張機能名と説明文をローカライズした
    git-svn-id: http://www.cozmixng.org/repos/piro/ezsidebar/trunk@569 599a83e7-65a4-db11-8015-0010dcdd6dc2

---
+++ defaults/preferences/ezsidebar.js
@@ -0,0 +1,2 @@
+pref("extensions.{0EAF175C-0C46-4932-AB7D-F45D6C46F367}.name", "chrome://ezsidebar/locale/ezsidebar.properties");
+pref("extensions.{0EAF175C-0C46-4932-AB7D-F45D6C46F367}.description", "chrome://ezsidebar/locale/ezsidebar.properties");
