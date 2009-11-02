27daf68e2988145f0e9011c044c4746f41b4ff88
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2009-11-02 01:21:42 +0000

    update for lately spec
    git-svn-id: http://www.cozmixng.org/repos/piro/ezsidebar/trunk@5313 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- defaults/preferences/ezsidebar.js
+++ defaults/preferences/ezsidebar.js
@@ -1,6 +1,3 @@
-pref("extensions.{0EAF175C-0C46-4932-AB7D-F45D6C46F367}.name", "chrome://ezsidebar/locale/ezsidebar.properties");
-pref("extensions.{0EAF175C-0C46-4932-AB7D-F45D6C46F367}.description", "chrome://ezsidebar/locale/ezsidebar.properties");
-
 pref("ezsidebar.datasource.file",                     "");
 pref("ezsidebar.shortcut.switchSidebarPanel.enabled", true);
 pref("ezsidebar.override.addPanelOperation",          true);
